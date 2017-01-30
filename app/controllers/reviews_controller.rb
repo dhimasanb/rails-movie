538fa473a68fdc8d4d1d2db92e70140bfdc021ab
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-01-30 09:31:55 +0700

    redirect to @movies

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,7 +5,7 @@ class ReviewsController < ApplicationController
 
   # GET /reviews
   # GET /reviews.json
-  
+
   # GET /reviews/1
   # GET /reviews/1.json
   def show
@@ -29,8 +29,8 @@ class ReviewsController < ApplicationController
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
-        format.json { render :show, status: :created, location: @review }
+        format.html { redirect_to @movie, notice: 'Review was successfully created.' }
+        format.json { render :show, status: :created, location: @movie }
       else
         format.html { render :new }
         format.json { render json: @review.errors, status: :unprocessable_entity }
