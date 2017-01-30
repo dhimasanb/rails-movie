8776e9ddb27b3554432d82a5cc899b10c6e857a0
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-01-30 08:30:38 +0700

    update resources nested reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :reviews
   devise_for :users
-  resources :movies
-  root to: "movies#index"   
+  resources :movies do
+    resources :reviews, except: [:show, :index]
+  end
+  root to: "movies#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
